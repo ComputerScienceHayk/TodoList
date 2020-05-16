6bf79cdd09a73e33f15f9f3fbdbab085986f8759
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-05-16 23:08:00 +0400

    Updates

---
+++ precache-manifest.18137b9e434307dbb785cbcbeac58717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d12483ea10e0888a9c7bbcf4419b87a",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "b0cb1cc50dce2eeafcf2",
+    "url": "/TodoList/static/css/main.706f0521.chunk.css"
+  },
+  {
+    "revision": "b3f1a71862e6a75a4a01",
+    "url": "/TodoList/static/js/2.36059b22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.36059b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec14414251ff67cd77b",
+    "url": "/TodoList/static/js/3.8fa5c6df.chunk.js"
+  },
+  {
+    "revision": "b0cb1cc50dce2eeafcf2",
+    "url": "/TodoList/static/js/main.3a397969.chunk.js"
+  },
+  {
+    "revision": "29532f8af7624bf3a8a7",
+    "url": "/TodoList/static/js/runtime-main.93328405.js"
+  }
+]);
